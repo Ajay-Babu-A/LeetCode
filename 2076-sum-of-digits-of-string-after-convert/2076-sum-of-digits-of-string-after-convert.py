4032554fce8ff9df8983776a07f1db433ccba719
Ajay-Babu-A <81210201+Ajay-Babu-A@users.noreply.github.com>
2024-09-03 21:34:11 +0530

    Time: 43 ms (21.53%), Space: 16.4 MB (93.62%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -1,14 +1,36 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         
-        str_num = ""
+        # Method 1:
+        # str_num = ""
 
-        for i in s:
-            str_num += str(ord(i) - ord('a') + 1)
+        # for i in s:
+        #     str_num += str(ord(i) - ord('a') + 1)
         
+        # for _ in range(k):
+
+        #     digit_sum = sum(int(value) for value in str_num)
+
+        #     str_num = str(digit_sum)
+        # return int(str_num)
+
+        # Method 2:
+
+        # find the ASCII value
+
+        def convert_num(s):
+            return ''.join(str(ord(i) - ord('a') + 1) for i in s)
+        
+        # sum of digits
+
+        def sum_digits(nums):
+            
+            return sum(int(d) for d in str(nums))
+
+        num = convert_num(s)
+
         for _ in range(k):
 
-            digit_sum = sum(int(value) for value in str_num)
-
-            str_num = str(digit_sum)
-        return int(str_num)
+            num = sum_digits(num)
+        
+        return num
